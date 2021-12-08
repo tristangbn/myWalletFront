08edc6db5d03d28698166ec2262ab0743433c785
François Cordier <francoiscordier.ece@gmail.com>
2021-12-08 18:08:11 +0100

    AddCryptoScreen search on lislist - HomeScreen button position

--- screens/AddCryptoScreen.js
+++ screens/AddCryptoScreen.js
@@ -1,76 +1,114 @@
 import React, { useEffect, useState } from "react";
-const axios = require("axios");
 import {
-  Button,
   Box,
-  Text,
-  Center,
-  NativeBaseProvider,
-  FormControl,
-  Select,
-  Container,
-  CheckIcon,
-  WarningOutlineIcon,
+  FlatList,
+  HStack,
+  Avatar,
   VStack,
-  Heading,
-  Spacer,
-  Typeahead,
+  Text,
+  Input,
   Icon,
+  Pressable,
 } from "native-base";
 import { connect } from "react-redux";
 import coinGeckoAPI from "../api/coinGecko";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import myWalletAPI from "../api/myWallet";
 
-function AddCrypto(props) {
-  const [service, setService] = useState("");
+const AddCryptoScreen = (props) => {
   const [coinList, setCoinList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [query, setQuery] = useState("");
   const token = props.authData[0].token;
 
   useEffect(() => {
     coinGeckoAPI
-      .get(
-        "/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-      )
-      .then(async (response) => {
+      .get("/coins/markets", {
+        params: {
+          vs_currency: "eur",
+          order: "market_cap_desc",
+          per_page: 100,
+          page: 1,
+          sparkline: false,
+        },
+      })
+      .then((response) => {
         setCoinList(response.data);
       });
-    // .catch((error) => {console.log(error)})
   }, []);
 
-  //Listing des principales cryptos pour affichage
-  const listCoin = coinList.map((coin, i) => {
-    return <Select.Item key={i} label={coin.name} value={coin.id} />;
-  });
+  const handleSearch = (query) => {
+    const formattedQuery = query.toLowerCase();
+    let filteredData = [];
+    if (query === "") {
+      filteredData = [];
+    } else {
+      filteredData = coinList.filter((crypto) => {
+        return crypto.id.includes(formattedQuery);
+      });
+    }
 
+    setFilteredList(filteredData);
+    setQuery(query);
+  };
 
-  //Valider l'ajout de la crypto et ajout dans une liste
-  const addCrypto = async () => {
-    console.log(service + " ajoutée");
+  const addCrypto = (id) => {
     myWalletAPI
       .post("/add-crypto", {
-        id : service,
+        id: id,
         token,
       })
-      .then(async function (response) {
-        console.log(response.data);
+      .then((response) => {
         props.navigation.navigate("bottomNav");
       });
+  };
 
+  const searchBar = () => {
+    return (
+      <VStack width="100%" space={5} alignItems="center" mb="5">
+        <Input
+          defaultValue={query}
+          onChangeText={(queryText) => handleSearch(queryText)}
+          placeholder="Search"
+          variant="filled"
+          width="100%"
+          bg="blueGray.800"
+          borderRadius="10"
+          py="1"
+          px="2"
+          placeholderTextColor="white"
+          _hover={{ bg: "gray.200", borderWidth: 0 }}
+          borderWidth="0"
+          _web={{
+            _focus: { style: { boxShadow: "none" } },
+          }}
+          InputLeftElement={
+            <Icon
+              ml="2"
+              size="5"
+              color="white"
+              as={<Ionicons name="ios-search" />}
+            />
+          }
+        />
+      </VStack>
+    );
   };
 
   return (
     <Box
+      safeArea
       alignItems="center"
       h="100"
       flex={1}
       px="3"
-      width="100%"
+      // width="100%"
       _dark={{ bg: "blueGray.900" }}
     >
       <Box
         p="10"
         pt="10"
-        mt="40px"
+        mt="5"
         placement="top"
         justifyContent="center"
         alignItems="center"
@@ -87,48 +125,66 @@ function AddCrypto(props) {
       >
         Add a crypto
       </Box>
+      <Box my="10" width="98%">
+        {searchBar()}
 
-      <VStack mt="20px" alignItems="center" space={4}>
-        <Select
-          selectedValue={service}
-          minWidth="90%"
-          accessibilityLabel="Select a crypto"
-          placeholder="Please select a crypto"
-          _selectedItem={{
-            bg: "teal.600",
-            endIcon: <CheckIcon size="5" />,
-          }}
-          mt={1}
-          onValueChange={(itemValue) => setService(itemValue)}
-        >
-          {listCoin}
-        </Select>
-      </VStack>
-
-      <Spacer />
-      <Button
-        mb="10"
-        placement="bottom"
-        width="98%"
-        _dark={{ bg: "violet.900" }}
-        rounded="lg"
-        onPress={() => console.log("hello world")}
-        _text={{
-          fontSize: "xl",
-          fontWeight: "medium",
-          color: "#ffffff",
-          letterSpacing: "lg",
-        }}
-        onPress={async () => addCrypto()}
-      >
-        Ajouter une crypto
-      </Button>
+        <FlatList
+          data={filteredList}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => addCrypto(item.id)}>
+              {({ isHovered, isPressed }) => {
+                return (
+                  <Box
+                    bg={
+                      isPressed
+                        ? "blueGray.800"
+                        : isHovered
+                        ? "cyan.800"
+                        : "blueGray.900"
+                    }
+                    p="3"
+                    rounded="8"
+                    style={{
+                      transform: [
+                        {
+                          scale: isPressed ? 0.96 : 1,
+                        },
+                      ],
+                    }}
+                  >
+                    <HStack space={3} alignItems="center">
+                      <Avatar
+                        size="30px"
+                        source={{
+                          uri: item.image,
+                        }}
+                      />
+                      <VStack>
+                        <Text
+                          _dark={{
+                            color: "white",
+                          }}
+                          color="coolGray.800"
+                          bold
+                        >
+                          {item.name}
+                        </Text>
+                      </VStack>
+                    </HStack>
+                  </Box>
+                );
+              }}
+            </Pressable>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </Box>
     </Box>
   );
-}
+};
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return { authData: state.authData };
-}
+};
 
-export default connect(mapStateToProps, null)(AddCrypto);
+export default connect(mapStateToProps, null)(AddCryptoScreen);
